56c99811789ca345a9e73ee2c917722086780e4b
yz <3424288785@qq.com>
2022-09-15 09:57:49 +0800

    Initial commit

---
+++ src/TwoSum/Solution.java
@@ -0,0 +1,22 @@
+package TwoSum;
+
+import java.util.Arrays;
+import java.util.Hashtable;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = {3, 3};
+        int target = 6;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        Hashtable<Integer, Integer> hashNums = new Hashtable<>();
+        for (int i=0;i<nums.length;i++) {
+            if (hashNums.containsKey(target - nums[i])){
+                return new int[]{i, hashNums.get(target - nums[i])};
+            }
+            hashNums.put(nums[i], i);
+        }
+        return new int[]{0, 0};
+    }
+}
